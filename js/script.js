7d20a5ef356e0c76f2db42ddee284f7369b5aa65
abirwan <abi.rahmawan01@student.uns.ac.id>
2024-12-18 09:42:17 +0700

    add banner autoslide

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 // Ini Javascript
+
+document.addEventListener('DOMContentLoaded', function () {
+    let currentIndex = 0;
+    const slides = document.querySelectorAll('.banner-slide');
+    const totalSlides = slides.length;
+
+    function showNextSlide() {
+        // Menyembunyikan gambar sebelumnya
+        slides[currentIndex].classList.remove('active');
+        
+        // Mengubah indeks gambar berikutnya
+        currentIndex = (currentIndex + 1) % totalSlides;
+        
+        // Menampilkan gambar berikutnya
+        slides[currentIndex].classList.add('active');
+    }
+
+    // Menampilkan gambar pertama
+    slides[currentIndex].classList.add('active');
+
+    // Menjalankan autoslide setiap 3 detik
+    setInterval(showNextSlide, 3000);
+});
